2ee8035165f69477ce013533fd0c2c8ea34a4d9f
Kholwani Mabhena <colwanymab@gmail.com>
2025-07-03 16:09:29 +0200

    one two

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,7 +5,7 @@ const Icons = ({name, image}) => {
     <a href={'https://appletree-mdu.vercel.app'}>
     <div className=' flex flex-col items-center'>
       <img className=' w-8' src={image} alt="" />
-      <p className=' text-sm text-center font-medium text-gray-700'>{name}</p>
+      <p className=' text-sm text-center font-medium text-gray-700'>K{name}</p>
     </div>
     </a>
   )
