c454ae1420acad0db86921eb686c73c29df99955
Kholwani Mabhena <colwanymab@gmail.com>
2025-07-03 16:08:16 +0200

    one two

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Icons = ({name, image}) => {
   return (
+    <a href={'https://appletree-mdu.vercel.app'}>
     <div className=' flex flex-col items-center'>
       <img className=' w-8' src={image} alt="" />
       <p className=' text-sm text-center font-medium text-gray-700'>{name}</p>
     </div>
+    </a>
   )
 }
 
--- src/components/IconsNavigation.jsx
+++ src/components/IconsNavigation.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 
 const IconsNavigation = ({image, name, active}) => {
   return (
+    <a href={'https://appletree-mdu.vercel.app'}>
     <div className={active ? ' flex text-blue-400 flex-col items-center' : ' flex text-gray-400 flex-col items-center'}>
       <img className=' w-8' src={image} alt="" />
       <p className=' text-sm text-center font-medium text-gray-500'>{name}</p>
     </div>
+    </a>
   )
 }
 
