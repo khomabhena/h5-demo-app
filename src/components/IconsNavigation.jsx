218aa6278edac8721b3b4eb0e4147e814a1e670f
Kholwani Mabhena <colwanymab@gmail.com>
2025-07-03 15:58:01 +0200

    one two

--- src/components/IconsNavigation.jsx
+++ src/components/IconsNavigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const IconsNavigation = ({image, name, active}) => {
   return (
+    <Link to={{pathname: 'https://appletree-mdu.vercel.app'}}>
     <div className={active ? ' flex text-blue-400 flex-col items-center' : ' flex text-gray-400 flex-col items-center'}>
       <img className=' w-8' src={image} alt="" />
       <p className=' text-sm text-center font-medium text-gray-500'>{name}</p>
     </div>
+    </Link>
   )
 }
 
