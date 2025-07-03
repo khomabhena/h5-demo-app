4eea2dce5922115563b63a1750faacd8c212bb3d
Kholwani Mabhena <colwanymab@gmail.com>
2025-07-03 17:10:39 +0200

    changed with to fit mobile screen

--- src/components/BottomDrawer.jsx
+++ src/components/BottomDrawer.jsx
@@ -5,10 +5,10 @@ const BottomDrawer = () => {
   return (
     <div className=' absolute shadow-2xl shadow-teal-900 bottom-0 left-0 right-0 min-h-12 bg-white'>
         <div className=' grid grid-cols-4 justify-start gap-2 my-4'>
-            <IconsNavigation name={'Home'} image={'./home.svg'}>
+            <IconsNavigation active={true} name={'Home'} image={'./home.svg'}>
             
             </IconsNavigation>
-            <IconsNavigation active={true} name={'Service'} image={'./service.svg'}>
+            <IconsNavigation name={'Service'} image={'./service.svg'}>
             </IconsNavigation>
             <IconsNavigation name={'Nearby'} image={'./nearby.svg'}>
                 
