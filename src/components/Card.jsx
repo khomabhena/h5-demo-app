64f5425396cb92fb550ed63f8aa6bb091579bad1
Kholwani Mabhena <colwanymab@gmail.com>
2025-07-03 16:02:29 +0200

    one two

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom'
 
 const Card = ({children, name}) => {
   return (
-    <Link to={{pathname: 'https://appletree-mdu.vercel.app'}}>
     <div className=' bg-white rounded-xl px-3 py-3 mt-4'>
       <p className=' text-lg font-bold'>{name}</p>
       <div className=' grid grid-cols-4 justify-start gap-2 my-4'>
         {children}
       </div>
     </div>
-    </Link>
+  
   )
 }
 
